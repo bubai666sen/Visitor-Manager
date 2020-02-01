cc935edb07b368563741458b41058dc79450883b
Sourav <bubai666sen@gmail.com>
2020-02-02 00:37:38 +0530

    feedback section

--- website/Contact/urls.py
+++ website/Contact/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('store-message',views.store_message,name="store_message"),
     path('',views.contact,name="contact"),
+    path('feedback',views.feedback,name="feedback"),
+    path('feedbacks',views.feedbacks,name="feedbacks"),
 ]
--- website/Contact/views.py
+++ website/Contact/views.py
@@ -9,6 +9,23 @@ def contact(request):
     }
     return render(request,'contact.html',data)
 
+def feedback(request):
+    data = {
+        'active':'feedback',
+    }
+    return render(request,'contact.html',data)
+
+def feedbacks(request):
+    try:
+        feedbacks = Message.objects.filter(typ="Feedback")
+    except:
+        feedbacks = []
+    data = {
+        'active':'feedback',
+        'feedbacks':feedbacks,
+    }
+    return render(request,'feedbacks.html',data)
+
 def store_message(request):
     message = Message(name=request.POST['name'],email=request.POST['email'],subject=request.POST['subject'],message=request.POST['message'],typ=request.POST['typ'],purpose=request.POST['purpose'])
     message.save()
